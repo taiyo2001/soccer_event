33c71d0ee92a065deff1496968d0e74ee1c2eb41
taiyo2001 <taiyo2778@gmail.com>
2024-01-30 12:30:42 +0900

    style: detail

--- app/services/master_data_generator/raw_zipcode_data_generator.rb
+++ app/services/master_data_generator/raw_zipcode_data_generator.rb
@@ -23,49 +23,5 @@ module MasterDataGenerator
         records.each_slice(1000, &RawZipcode.method(:import!))
       end
     end
-
-    # def self.generate_add_rawdata(diff_date = last_month_diff_date)
-    #   file_name = "add_#{diff_date}"
-    #   raise Exception, "#{file_name} has already added" if RawAddZipcode.exists?(file_name:)
-
-    #   ActiveRecord::Base.transaction do
-    #     CSV.foreach(ZipcodeDownloader.new.download_diff_file(file_name), headers: false, encoding: 'Shift_JIS') do |row|
-    #       RawAddZipcode.create(
-    #         file_name:,
-    #         code: row[2],
-    #         prefecture_kana: row[3],
-    #         city_kana: row[4],
-    #         town_kana: row[5],
-    #         prefecture: row[6],
-    #         city: row[7],
-    #         town: row[8]
-    #       )
-    #     end
-    #   end
-    # end
-
-    # def self.generate_del_rawdata(diff_date = last_month_diff_date)
-    #   file_name = "del_#{diff_date}"
-    #   raise Exception, "#{file_name} has already added" if RawDeleteZipcode.exists?(file_name:)
-
-    #   ActiveRecord::Base.transaction do
-    #     CSV.foreach(ZipcodeDownloader.new.download_diff_file(file_name), headers: false, encoding: 'Shift_JIS') do |row|
-    #       RawDeleteZipcode.create(
-    #         file_name:,
-    #         code: row[2],
-    #         prefecture_kana: row[3],
-    #         city_kana: row[4],
-    #         town_kana: row[5],
-    #         prefecture: row[6],
-    #         city: row[7],
-    #         town: row[8]
-    #       )
-    #     end
-    #   end
-    # end
-
-    # def self.last_month_diff_date
-    #   (Time.current - 1.month).strftime('%Y%m').to_i - 200_000
-    # end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
-# !/usr/bin/env ruby
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
 
 puts 'Please wait a moment for the insertion of the address'
 
