35af54d9007e9de110fc5eef1b35da94957fb989
taiyo2001 <taiyo2778@gmail.com>
2024-01-30 12:23:38 +0900

    chore: update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_29_072640) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_29_081518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.string "kana"
+    t.bigint "prefecture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["prefecture_id"], name: "index_cities_on_prefecture_id"
+  end
+
   create_table "leagues", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "prefectures", force: :cascade do |t|
+    t.string "name"
+    t.string "kana"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "raw_zipcodes", force: :cascade do |t|
+    t.string "code"
+    t.string "prefecture"
+    t.string "prefecture_kana"
+    t.string "city"
+    t.string "city_kana"
+    t.string "town"
+    t.string "town_kana"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -26,6 +54,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_29_072640) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["league_id"], name: "index_teams_on_league_id"
+  end
+
+  create_table "towns", force: :cascade do |t|
+    t.string "name"
+    t.string "kana"
+    t.bigint "city_id"
+    t.string "zipcode_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_towns_on_city_id"
+    t.index ["zipcode_id"], name: "index_towns_on_zipcode_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,6 +84,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_29_072640) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "zipcodes", id: :string, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "cities", "prefectures"
   add_foreign_key "teams", "leagues"
+  add_foreign_key "towns", "cities"
+  add_foreign_key "towns", "zipcodes"
   add_foreign_key "users", "teams", column: "favorite_team_id"
 end
