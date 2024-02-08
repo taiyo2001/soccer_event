8a6234d933631bcad8fc349d3832a1d340801fed
yocchan-git <yocchan.business@gmail.com>
2024-02-08 11:32:59 +0900

    本番環境用のdevelop設定にする

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -68,8 +68,16 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :letter_opener_web
-
-  # TODO: 必要なら本番環境用にメールの設定をする
+  # TODO: アプリのURLを本番のドメインに変更する
+  config.action_mailer.default_url_options = { protocol: 'https', host:'アプリのURL'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            ENV.fetch('YOUR_GMAIL_ADDRESS', nil),
+    password:             ENV.fetch('YOUR_GMAIL_PASSWORD', nil),
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
 end
