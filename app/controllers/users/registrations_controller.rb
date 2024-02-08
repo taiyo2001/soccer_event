d1f157f56c22141e0cd83e27a4a5a4650986a37d
yocchan-git <yocchan.business@gmail.com>
2024-02-08 13:43:43 +0900

    update後の遷移を確認する

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
+  # メソッド定義だけでupdate後はユーザ詳細に遷移する
   def after_update_path_for(_resource)
-    # TODO: アップデート後の遷移先を指定する
   end
 end
