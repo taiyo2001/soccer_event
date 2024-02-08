b91fe33a1aeafbaf45a4637c4a2a4defbc64acc2
yocchan-git <yocchan.business@gmail.com>
2024-02-08 13:47:12 +0900

    rubocopを通す

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   # メソッド定義だけでupdate後はユーザ詳細に遷移する
-  def after_update_path_for(_resource)
-  end
+  def after_update_path_for(_resource); end
 end
